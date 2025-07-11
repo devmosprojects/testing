9ef624a44f9ce7b836d9f6b57cde81c818d902bf
unknown <sravanthi.bokkala12@gmail.com>
2025-07-11 03:08:09 -0700

    first commit

---
+++ app.java
@@ -0,0 +1,7 @@
+public class DevaTechnosoft {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Deva Technosoft!");
+        System.out.println("We provide IT solutions and services.");
+        System.out.println("Contact us at: info@devatechnosoft.com");
+    }
+}
